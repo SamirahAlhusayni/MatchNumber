d6aa9b4db284338c3741a9504f297a12718cc8b4
SamirahAlhusayni <samirah.alhusayni@tuwaiq.edu.sa>
2021-04-19 06:04:26 +0300

    MatchNumer Ex solved by stack

---
+++ MatchNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+//to use stack
+namespace MatchNumber
+{
+    class Program
+    {
+        static bool match(string input)
+            //Will return "true" if there is a pair of each input number, otherwise "false"
+        {
+            Stack<int> stack = new Stack<int>(); 
+            foreach (char ch in input)
+            {
+                if (stack.Count == 0 || ch != stack.Peek())
+                {
+                    stack.Push(ch);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+                if (stack.Count == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--- Hello , Please enter the number series you want to test --- ");
+            string test = Console.ReadLine();
+
+            Console.WriteLine("Result: {0}" , match(test));
+        }
+    }
+}
